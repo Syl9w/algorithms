4fd41269e876f6ebbef4d9eb396d46f954865cfc
Syl9w <ahmetkalievsultan@gmail.com>
2021-05-28 17:22:17 +0200

    Initial Commit

---
+++ fp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  fp
+//
+//  Created by MAC on 28/05/2021.
+//  Copyright © 2021 MAC. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
